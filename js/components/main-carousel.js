1309a8460ad2ffdccf7edbded055bad7ed35cd9c
Erick Costa <deoliveira.erick@gmail.com>
2021-05-01 16:09:09 -0300

    Create JavaScript of the carousel

---
+++ js/components/main-carousel.js
@@ -0,0 +1,26 @@
+// 1 Acesar a Janela (browser)
+// 2 Pegar o HTML todo
+// 3 Pegar botão
+// 4 Saber que está sendo clicado no botão
+
+// 5 Acesar a Janela (browser)
+// 6 Pegar o HTML todo
+// 7 Pegar o elements
+// 8 Mover o elements para direita
+
+
+const btnRight = document.querySelector('.button-arrow.-right');
+const btnLeft = document.querySelector('.button-arrow.-left');
+const elements = document.querySelector('.elements');
+let pixels = 400;
+
+btnRight.addEventListener('click', function() {
+    pixels = pixels - 50
+    elements.style = `transform: translateX(${pixels}px)`
+});
+
+btnLeft.addEventListener('click', function() {
+    pixels = pixels + 50
+    elements.style = `transform: translateX(${pixels}px)`
+});
+
